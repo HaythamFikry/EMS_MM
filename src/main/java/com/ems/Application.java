de05a66c44f9138bf6c59a22dd0cdabafa501206
Haytham Fikry <HaythamFikry@Gmail.com>
2025-04-22 12:09:15 +0200

    EMS

--- src/main/java/com/ems/Application.java
+++ src/main/java/com/ems/Application.java
@@ -5,8 +5,8 @@ import com.ems.services.EmailService;
 public class Application {
     public static void main(String[] args) {
         EmailService emailService = new EmailService();
-        emailService.sendEmail("hfadaly@tech-hub.tech","© 2025 Event Management System","Test The Email Service \n" +"\n"+
+        emailService.sendEmail("EventManagementSystem.Team@gmail.com","© 2025 Event Management System","Test The Email Service \n" +"\n"+
                 "Kind regards,\n" +
-                "Ahmed Soufi\n");
+                "Event Management System Team\n");
     }
 }
