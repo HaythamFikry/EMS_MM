ed372072e6192552760283790941bc2f0423369d
Ahmed Soliman <ahmedsoliman202@gmail.com>
2025-05-07 03:13:51 +0300

    seprate Base URL and other Endpoints in config.json , and refactored the webdriver setup, load and save to new config.json

--- tests/login_ui.py
+++ tests/login_ui.py
@@ -2,38 +2,48 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.chrome.options import Options
+import time,json,urllib
 
 class MyEventsPageTest(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("http://localhost:8080/EventManagementSystemV2/login")
+    @classmethod
+    def setUp(cls):
+        with open("tests/config.json") as f:
+            config = json.load(f)
+            cls.BASE_URL = config["BASE_URL"]
+            cls.LOGIN_URL = urllib.parse.urljoin(cls.BASE_URL, config["LOGIN_URL"])
+        
+        cls.options = Options()
+        cls.options.add_argument("--start-maximized")
+
+        driver = webdriver.Chrome(options=cls.options)
+        cls.driver.get(cls.LOGIN_URL)
 
     def test_successful_login(self):
-        driver = self.driver
-
-        username = driver.find_elements(By.CSS_SELECTOR, 'input[name="username"]')
+        self.driver.get(self.LOGIN_URL)
+        username = self.driver.find_elements(By.CSS_SELECTOR, 'input[ui_test="login-username"]')
         self.assertTrue(username, "Username field not found")
         username[0].send_keys("soliman")
 
-        password = driver.find_elements(By.CSS_SELECTOR, 'input[name="password"]')
+        password = self.driver.find_elements(By.CSS_SELECTOR, 'input[ui_test="login-password"]')
         self.assertTrue(password, "Password field not found")
-        password[0].send_keys("soliman")
+        password[0].send_keys("123")
         password[0].send_keys(Keys.RETURN)
 
         time.sleep(2)
 
-        welcome_div = driver.find_elements(By.CSS_SELECTOR, 'div.welcome')
+        welcome_div = self.driver.find_elements(By.CSS_SELECTOR, 'div.welcome')
         self.assertTrue(welcome_div, "Welcome message not found after login")
 
     def test_failed_login_invalid_credentials(self):
-        driver = self.driver
+        
+        self.driver.get(self.LOGIN_URL)
 
-        username = driver.find_elements(By.CSS_SELECTOR, 'input[name="username"]')
+        username = self.driver.find_elements(By.CSS_SELECTOR, 'input[ui_test="login-username"]')
         self.assertTrue(username, "Username field not found")
         username[0].send_keys("wronguser")
 
-        password = driver.find_elements(By.CSS_SELECTOR, 'input[name="password"]')
+        password = self.driver.find_elements(By.CSS_SELECTOR, 'input[ui_test="login-password"]')
         self.assertTrue(password, "Password field not found")
         password[0].send_keys("wrongpassword")
         password[0].send_keys(Keys.RETURN)
@@ -41,11 +51,13 @@ class MyEventsPageTest(unittest.TestCase):
         time.sleep(2)
 
         # Check if error message is displayed
-        error_div = driver.find_elements(By.CSS_SELECTOR, 'div.alert.alert-danger[role="alert"]')
+        error_div = self.driver.find_elements(By.CSS_SELECTOR, 'div.alert.alert-danger[role="alert"]')
         self.assertTrue(error_div, "Error message not displayed for invalid login")
 
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
--- ui_tests/venue_ui.py
+++ ui_tests/venue_ui.py
@@ -1,22 +1,36 @@
-import unittest, time, faker, random
+import unittest, time, faker, random,json,urllib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 class TestAddVenueForm(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome()
-        cls.driver.get("http://127.0.0.1:8080/EventManagementSystemV2/login")
+        
+        with open("tests/config.json") as f:
+            config = json.load(f)
+            cls.BASE_URL = config["BASE_URL"]
+            cls.LOGIN_URL = urllib.parse.urljoin(cls.BASE_URL, config["LOGIN_URL"])
+            cls.LIST_VENUE_URL = urllib.parse.urljoin(cls.BASE_URL, config["LIST_VENUE_URL"])
+            cls.ADD_VENUE_URL = urllib.parse.urljoin(cls.BASE_URL, config["ADD_VENUE_URL"])
 
+
+        cls.options = Options()
+        cls.options.add_argument("--start-maximized")
+
+        cls.driver = webdriver.Chrome(options=cls.options)
+        cls.driver.get(cls.LOGIN_URL)
+
+        time.sleep(1)
         # Log in the user
-        username = cls.driver.find_elements(By.CSS_SELECTOR, 'input[name="username"]')
+        username = cls.driver.find_elements(By.CSS_SELECTOR, 'input[ui_test="login-username"]')
         cls.assertTrue(username, "Username field not found")
         username[0].send_keys("soliman")
 
-        password = cls.driver.find_elements(By.CSS_SELECTOR, 'input[name="password"]')
+        password = cls.driver.find_elements(By.CSS_SELECTOR, 'input[ui_test="login-password"]')
         cls.assertTrue(password, "Password field not found")
         password[0].send_keys("123")
         password[0].send_keys(Keys.RETURN)
@@ -28,26 +42,26 @@ class TestAddVenueForm(unittest.TestCase):
 
 
 
-    def test_add_venue_redirect(self):
-        print("test_add_venue_redirect")
-        self.driver.get("http://127.0.0.1:8080/EventManagementSystemV2/venues")
-        # Wait for the page to redirect
-        time.sleep(2)
+    # def test_add_venue_redirect(self):
+    #     print("test_add_venue_redirect")
+    #     self.driver.get(self.LIST_VENUE_URL)
+    #     # Wait for the page to redirect
+    #     time.sleep(2)
 
-        # Click the add button
-        add_button = self.driver.find_element(By.XPATH, "//a[@ui_test='add-venue-button']")
-        add_button.click()
+    #     # Click the add button
+    #     add_button = self.driver.find_element(By.XPATH, "//a[@ui_test='add-venue-button']")
+    #     add_button.click()
 
-        # Wait for the page to redirect
-        time.sleep(2)
-        # Assert that the URL is correct
-        print(self.driver.current_url)
-        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/EventManagementSystemV2/venues/add")
+    #     # Wait for the page to redirect
+    #     time.sleep(2)
+    #     # Assert that the URL is correct
+    #     print(self.driver.current_url)
+    #     self.assertEqual(self.driver.current_url, self.ADD_VENUE_URL)
     
     def test_add_5_venues(self):
         fake = faker.Faker()        
         venues = []
-        for _ in range(5):
+        for _ in range(3):
             venue = {
                 "name": fake.company(),
                 "address": fake.address().replace("\n", " "),
@@ -59,8 +73,9 @@ class TestAddVenueForm(unittest.TestCase):
             venues.append(venue)
 
         for venue in venues:
-            self.driver.get("http://127.0.0.1:8080/EventManagementSystemV2/venues/add")
-            print(venue)
+            self.driver.get(self.ADD_VENUE_URL)
+            
+            time.sleep(2)
             # Fill in the form inputs
             name_input = self.driver.find_element(By.CSS_SELECTOR, "[ui_test='add-venue-name']")
             name_input.send_keys(venue["name"])
@@ -80,14 +95,14 @@ class TestAddVenueForm(unittest.TestCase):
             contact_email_input.send_keys(venue["contact_email"])
 
             # Click the submit button
-            submit_button = self.driver.find_element(By.XPATH, "//button[@ui_test='add-venue-button']")
-            submit_button.click()
+            submit_button = self.driver.find_element(By.XPATH, "//button[@ui_test='submit-venue-button']")
+            self.driver.execute_script("arguments[0].click();", submit_button)
 
             # Wait for the page to redirect
-            time.sleep(1)
+            time.sleep(2)
             # Assert that the URL is correct
             print(self.driver.current_url)
-            self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/EventManagementSystemV2/venues")
+            self.assertEqual(self.driver.current_url, self.LIST_VENUE_URL)
 
             # Assert that the inserted title is present
             # title_element = self.driver.find_element(By.XPATH, "//div[@class='venue-card-text']/h5[contains(., 'Test Venue')]")
